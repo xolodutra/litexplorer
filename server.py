a8333f9c327fb64622ef71fc2f6baf89ba8106d8
xolodutra <xolodutra@gmail.com>
2020-10-02 21:06:11 +0300

    Я короч выснил, что за косяк был. Я забыл "@" в декораторе указать. Буду внимательнее впредь

--- server.py
+++ server.py
@@ -3,14 +3,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 @app.route("/home")
 def index():
     return "Hi!"
 
-app.route("/news/<id>")
+
+@app.route("/news/<id>")
 def news_by_id(id):
-    return "GGGGG: " + id
+    return 'Hi: ' + id
+
 
 if __name__ == "__main__":
     app.run(port=5010, debug=True)
