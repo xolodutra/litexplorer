08a2b7cd6b9f45dc0ad29e011270135d788655c6
xolodutra <xolodutra@gmai.com>
2020-10-02 18:36:54 +0300

    две страницы в приложении Одна не работает так как надо

--- server.py
+++ server.py
@@ -2,9 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/test")
+@app.route("/")
 def index():
     return "Hi!"
 
+app.route("/news/<news_id>")
+def news_by_id(news_id):
+    return 'Новость: ' + news_id
+
 if __name__ == "__main__":
-    app.run()
+    app.run(port=5010, debug=True)
