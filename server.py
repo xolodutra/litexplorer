f26a080d788f98b1aedf2b3773a62b4eee98df19
xolodutra <xolodutra@gmai.com>
2020-10-02 19:45:14 +0300

    что-то нихера не получается

--- server.py
+++ server.py
@@ -1,14 +1,16 @@
 from flask import Flask
 
+
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def index():
     return "Hi!"
 
-app.route("/news/<news_id>")
-def news_by_id(news_id):
-    return 'Новость: ' + news_id
+app.route("/news/<id>")
+def news_by_id(id):
+    return "GGGGG: " + id
 
 if __name__ == "__main__":
     app.run(port=5010, debug=True)
